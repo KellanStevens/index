cc7ee361faa772a28cacb7457a0dc778189d6a65
Kellan Stevens <github@kellanstevens.com>
2026-03-23 17:23:02 +0200

    refactor: theme-init script to ts

--- src/scripts/theme-init.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    var stored = localStorage.getItem('theme');
-    var prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    var shouldBeDark = stored === 'dark' || (stored !== 'light' && prefersDark);
-    if (shouldBeDark) {
-        document.documentElement.classList.add('dark');
-    }
-})();
---
+++ src/scripts/theme-init.ts
@@ -0,0 +1,11 @@
+(function () {
+    const stored = localStorage.getItem('theme');
+    const prefersDark = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+    ).matches;
+    const shouldBeDark =
+        stored === 'dark' || (stored !== 'light' && prefersDark);
+    if (shouldBeDark) {
+        document.documentElement.classList.add('dark');
+    }
+})();
